2f6975d5bcde8f785eae89603e39086a1ecffc08
thalals <thalsal@naver.com>
2021-04-17 23:43:19 +0900

    [UPD] ViewFilipper

--- androidstudio_school/61ViewContainer/app/src/main/java/com/example/a6_1viewcontainer/MainActivity.java
+++ androidstudio_school/61ViewContainer/app/src/main/java/com/example/a6_1viewcontainer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a6_1viewcontainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPrev, btnNext;
+        final ViewFlipper vFlipper;
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        vFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                vFlipper.showPrevious();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                vFlipper.showNext();
+            }
+        });
     }
 }
