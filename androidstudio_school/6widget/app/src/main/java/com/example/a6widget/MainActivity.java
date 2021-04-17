86db856b6d87d6e5360380333ab3d592ca54946b
thalals <thalsal@naver.com>
2021-04-17 23:02:16 +0900

    [UPD] Create Calender Widget

--- androidstudio_school/6widget/app/src/main/java/com/example/a6widget/MainActivity.java
+++ androidstudio_school/6widget/app/src/main/java/com/example/a6widget/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.a6widget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chrono;
+    Button btnStart, btnEnd;
+    RadioButton rdoCal,rdoTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvTime, tvMin;
+
+    int selectYear, selectMonth, selectDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("시간 예약");
+
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnEnd = (Button)findViewById(R.id.btnEnd);
+
+        chrono = (Chronometer)findViewById(R.id.chronometer1);
+
+        rdoCal=(RadioButton)findViewById(R.id.rdoCa1);
+        rdoTime=(RadioButton)findViewById(R.id.rdoTime);
+
+        tPicker=(TimePicker)findViewById(R.id.timePicker1);
+        calView=(CalendarView)findViewById(R.id.calendarView1);
+
+        tvYear=(TextView)findViewById(R.id.tvYear);
+
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                tPicker.setVisibility(View.VISIBLE);
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+            }
+        });
+
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectYear=year;
+                selectMonth=month+1;
+                selectDay=dayOfMonth;
+            }
+        });
+
+        tvYear.setText(Integer.toString(selectYear));
+        tvMonth.setText(Integer.toString(selectMonth));
+        tvDay.setText(Integer.toString(selectDay));
+        tvTime.setText(Integer.toString(tPicker.getCurrentHour()));
+        tvMin.setText(Integer.toString(tPicker.getCurrentMinute()));
+
     }
 }
